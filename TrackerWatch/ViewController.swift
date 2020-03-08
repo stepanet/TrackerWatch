ea1d65abac97841e481df19f7f943a25ec4196aa
stepanet <stepanet@gmail.com>
2020-03-08 11:29:44 +0300

    Initial Commit

---
+++ TrackerWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackerWatch
+//
+//  Created by Jack Sp@rroW on 08.03.2020.
+//  Copyright © 2020 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
